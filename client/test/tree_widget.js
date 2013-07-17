3dd7c4be228c5cb3a425165c02f5532fee9cd501
Jean-Louis GUENEGO <jlguenego@gmail.com>
2013-07-17 19:04:32 +0200

    tree

--- client/test/tree_widget.js
+++ client/test/tree_widget.js
@@ -11,18 +11,27 @@
 $.widget( "ui.ocp_tree", {
 	version: "0.0.1",
 	options: {
-		source: []
+		source: [],
 	},
 
 	_create: function() {
-		this._fill(this.element, this.options.source, 0);
+		this._fill(this.element, this.options.source, 0, false);
+
+        this._on( $('.tree_toggle'), {
+          click: "toggle"
+        });
+
+		var self = this;
+		$('.tree_toggle').each(function() {
+			self.toggle({ currentTarget: this});
+		});
 		return this;
 	},
 
 	_destroy: function() {
 	},
 
-	_fill: function(el, src, level) {
+	_fill: function(el, src, level, last) {
 		var ul = $('<div class="tree_struct"/>').appendTo(el);
 		for (var i = 0; i < src.length; i++) {
 			console.log('coucou ' + level);
@@ -30,13 +39,17 @@ $.widget( "ui.ocp_tree", {
 			var li = $('<div/>').appendTo(ul);
 			var row = $('<div class="tree_row"/>').appendTo(li);
 			for (var j = 0; j < level; j++) {
-				row.append('<img src="image/elbow-line.png"/>');
+				if (last) {
+					row.append('<img src="image/elbow-blank.png"/>');
+				} else {
+					row.append('<img src="image/elbow-line.png"/>');
+				}
 			}
 			if (item.children && item.children.length > 0) {
 				if (i == src.length - 1) {
-					row.append('<img src="image/elbow-end-minus.png"/>');
+					row.append('<img class="tree_toggle" src="image/elbow-end-minus.png"/>');
 				} else {
-					row.append('<img src="image/elbow-minus.png"/>');
+					row.append('<img class="tree_toggle" src="image/elbow-minus.png"/>');
 				}
 			} else {
 				if (i == src.length - 1) {
@@ -48,9 +61,29 @@ $.widget( "ui.ocp_tree", {
 
 			row.append('<div class="tree_text">' + level + ' ' + item.item.label + '</div>');
 			if (item.children && item.children.length > 0) {
-				this._fill(li, item.children, level + 1);
+				if (i == src.length - 1) {
+					this._fill(li, item.children, level + 1, true);
+				} else {
+					this._fill(li, item.children, level + 1, false);
+				}
 			}
 		}
+	},
+
+	toggle: function(event) {
+		var tree_struct = $(event.currentTarget).parent().parent().find('.tree_struct');
+		tree_struct.toggle();
+		var image_src = $(event.currentTarget).attr("src");
+		if (image_src == "image/elbow-end-minus.png") {
+			image_src = "image/elbow-end-plus.png";
+		} else if (image_src == "image/elbow-minus.png") {
+			image_src = "image/elbow-plus.png";
+		} else if (image_src == "image/elbow-end-plus.png") {
+			image_src = "image/elbow-end-minus.png";
+		} else if (image_src == "image/elbow-plus.png") {
+			image_src = "image/elbow-minus.png";
+		}
+		$(event.currentTarget).attr("src", image_src);
 	}
 });
 
