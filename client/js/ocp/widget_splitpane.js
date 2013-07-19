a9aacad14f099b87fe1a11409f53ed8dc56228cc
THOMIAS Yannis <thomias.yannis@gmail.com>
2013-07-19 16:23:38 +0200

    add template greed & splitpane

---
+++ client/js/ocp/widget_splitpane.js
@@ -0,0 +1,26 @@
+/*!
+ * jQuery UI Tree
+ *
+ *
+ * Depends:
+ *   jquery.ui.core.js
+ *   jquery.ui.widget.js
+ */
+(function( $, undefined ) {
+
+$.widget( "ui.ocp_splitpane", {
+	version: "0.0.1",
+	options: {
+		source: []
+	},
+
+	_create: function() {
+		// TODO: to be implemented.
+		return this;
+	},
+
+	_destroy: function() {
+	},
+});
+
+})( jQuery );
