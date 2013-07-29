4ea8a131f3bb7d53ad8763959c5671f140d4ebf8
THOMIAS Yannis <thomias.yannis@gmail.com>
2013-07-29 12:56:36 +0200

    Ajax

--- server/endpoint/index.php
+++ server/endpoint/index.php
@@ -1,15 +1,16 @@
 <?php
 	header("Content-Type:text/plain");
+	//sleep(2);
 ?>
 [
 	{
-		name: 'hello',
-		label: 'Hello',
-		type: 'dir'
+		"name": "hello",
+		"label": "Hello",
+		"type": "dir"
 	},
 	{
-		name: 'world',
-		label: 'World',
-		type: 'dir'
+		"name": "world",
+		"label": "World",
+		"type": "dir"
 	}
 ]
