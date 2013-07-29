7c714b9b2c21f6165c1501bf72de3857deea5937
THOMIAS Yannis <thomias.yannis@gmail.com>
2013-07-29 14:31:06 +0200

    Work with file:///

--- server/endpoint/index.php
+++ server/endpoint/index.php
@@ -1,6 +1,16 @@
 <?php
+	header('Access-Control-Allow-Origin: *');
 	header("Content-Type:text/plain");
+	define('ROOT', '../clt_root');
 	//sleep(2);
+	$dir = dir(ROOT.$_GET['path']);
+
+	//List files in images directory
+	while (($file = $dir->read()) !== false) {
+		echo "filename: " . $file . "<br />";
+	}
+
+	$dir->close();
 ?>
 [
 	{
