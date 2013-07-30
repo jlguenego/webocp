b22d2580b3a7989a7b6817296fa99bd1e9887c13
THOMIAS Yannis <thomias.yannis@gmail.com>
2013-07-30 11:55:02 +0200

    Widget menu

--- client/js/ocp/widget_menu.js
+++ client/js/ocp/widget_menu.js
@@ -14,98 +14,83 @@
 $.widget( "ui.ocp_menu", {
 	version: "0.0.1",
 	options: {
-		data_id: null,
+		body_id: null,
 	},
 
 	menu_body: null,
 	menu_transition: null,
 
 	_create: function() {
-		var data_id = this.options.data_id || this.element.attr('id') + '_data';
-		this.menu_body = $('#' + data_id);
+		var body_id = this.options.body_id || this.element.attr('id') + '_body';
+		this.menu_body = $('#' + body_id);
 		this.menu_body.hide();
-
-		console.log(this.element);
 
 		this.element.addClass('widget_menu_root');
 		this.menu_body.addClass('widget_menu_body');
 
 		this.menu_transition = $('<div/>').outerWidth(this.element.outerWidth());
 		this.menu_transition.addClass('widget_menu_transition');
-		this.menu_transition.insertBefore(this.menu_body);
 		this.menu_transition.append('<div class="widget_menu_arrow"/>');
 		this.menu_transition.hide();
-
+		this.menu_transition.insertBefore(this.menu_body);
 
 		var self = this;
 		this.element.mouseover(function() {
-			self._set_body_offset();
-			self.menu_body.show();
-			self.menu_transition.show();
+			self._show_menu();
 		}).mouseout(function(e){
-			self.menu_body.hide();
-			self.menu_transition.hide();
+			self._hide_menu();
 		});
 
 		this.menu_transition.mouseover(function() {
-			self._set_body_offset();
-			self.menu_body.show();
-			self.menu_transition.show();
+			self._show_menu();
 		}).mouseout(function(e){
-			self.menu_body.hide();
-			self.menu_transition.hide();
+			self._hide_menu();
 		});
 
 		this.menu_body.mouseover(function() {
-			self._set_body_offset();
-			self.menu_body.show();
-			self.menu_transition.show();
+			self._show_menu();
 		}).mouseout(function(e){
-			self.menu_body.hide();
-			self.menu_transition.hide();
+			self._hide_menu();
 		});
 
 		return this;
 	},
 
-	is_mouse_inside: function(el, e) {
-		var offset = el.offset();
-		offset.bottom = offset.top + el.outerHeight();
-		offset.right = offset.left + el.outerWidth();
-		console.log(offset);
-		if (offset.top < e.pageY && e.pageY > offset.bottom) {
-			return false;
-		}
-		if (offset.left < e.pageX && e.pageX > offset.right) {
-			return false;
-		}
-		return true;
-	},
-
 	_destroy: function() {
 	},
 
+	_show_menu: function() {
+		this._set_body_offset();
+		this.menu_body.show();
+		this.menu_transition.show();
+	},
+
+	_hide_menu: function() {
+		this.menu_body.hide();
+		this.menu_transition.hide();
+	},
+
 	_set_body_offset: function() {
-		var offset = this.element.offset();
-		offset.top = offset.top + this.element.outerHeight();
+		var menu_body_offset = this.element.offset();
+		menu_body_offset.top = menu_body_offset.top + this.element.outerHeight();
 
 		var width = this.menu_body.outerWidth();
-		var body_offset_right = offset.left + width;
+		var body_offset_right = menu_body_offset.left + width;
 		var window_w = this.element.parent().width();
 		if (body_offset_right > window_w) {
 			body_offset_right -= (body_offset_right - window_w);
-			offset.left = body_offset_right - width;
+			menu_body_offset.left = body_offset_right - width;
 		}
 
 		this.menu_body.css({
 			position: 'absolute',
-			top: offset.top + 8,
-			left: offset.left
+			top: menu_body_offset.top + 8,
+			left: menu_body_offset.left
 		});
 
 		this.menu_transition.css({
 			position: 'absolute',
-			top: offset.top - 5,
+			top: menu_body_offset.top - 5,
 			left: this.element.offset().left
 		});
 	}
