754cdbea15b28b8eaa2f048a3fa840241ed9c0d1
Jean-Louis GUENEGO <jlguenego@gmail.com>
2013-09-17 13:53:25 +0200

    Download file

--- server/original/endpoint/download.php
+++ server/original/endpoint/download.php
@@ -5,18 +5,31 @@
 	require_once(BASE_DIR . '/include/misc.inc');
 	require_once(BASE_DIR . '/include/constant.inc');
 
-	$path = $_GET['path'];
-	debug('path=' . $path);
-	$name = basename($path);
-	$filename = ROOT . $path;
-	$fp = fopen($filename, 'rb');
-	header('Access-Control-Allow-Origin: *');
-	header('Content-Disposition: attachment; filename="'.$name.'"');
-	header("Content-Transfer-Encoding: binary");
-	header('Accept-Ranges: bytes');
-	header("Cache-control: private");
-	header('Pragma: private');
-	header('Content-Length: ' . filesize($filename));
-	fpassthru($fp);
-	exit;
+	try {
+		if (!isset($_GET['path'])) {
+			throw new Exception('path not found.');
+		}
+		$path = $_GET['path'];
+		debug('path=' . $path);
+		$name = basename($path);
+		$filename = ROOT . $path;
+		if (!is_file($filename)) {
+			throw new Exception('file does not exist.');
+		}
+		$fp = fopen($filename, 'rb');
+		header('Access-Control-Allow-Origin: *');
+		header('Content-Disposition: attachment; filename="'.$name.'"');
+		header("Content-Transfer-Encoding: binary");
+		header('Accept-Ranges: bytes');
+		header("Cache-control: private");
+		header('Pragma: private');
+		header('Content-Length: ' . filesize($filename));
+		fpassthru($fp);
+		exit;
+	} catch (Exception $e) {
+		$output = array();
+		$output['error'] = $e->getMessage();
+		$result = json_encode($output);
+		echo $result;
+	}
 ?>
