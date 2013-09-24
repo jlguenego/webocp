37362d7ef5fc8f1fb7f73b9c8c95564b492fad32
THOMIAS Yannis <thomias.yannis@gmail.com>
2013-09-24 09:07:11 +0200

    Enhance contact list

--- server/test/endpoint/get_contact_list.php
+++ server/test/endpoint/get_contact_list.php
@@ -15,13 +15,17 @@
 	$_REQUEST = array_merge($_GET, $_POST);
 	$output = array();
 	try {
-		$contact_list = OCP::get_contact_list();
+		$new_contact = json_decode($_REQUEST['contact']);
+		$ocp = new OCP();
+		$ocp->load(OCP::get_name_from_url($_SERVER['REQUEST_URI']));
 		print_r($contact_list);
-		$output['result']['contact_list'] = $contact_list;
+		$name = $ocp->name;
+		$ocp->contact_list->$name = $ocp->to_contact();
+		$output['result'] = $ocp->contact_list;
 	} catch (Exception $e) {
 		$output['error'] = $e->getMessage();
 	}
 	debug_r('OUTPUT', $output);
-	$result = json_encode($output);
+	$result = prettyPrint(json_encode($output));
 	echo $result;
 ?>
