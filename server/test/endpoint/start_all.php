392ab013fb12df7d0f735b2192580f9d951b6476
THOMIAS Yannis <thomias.yannis@gmail.com>
2013-09-24 09:01:29 +0200

    Start n nodes

--- server/test/endpoint/start_all.php
+++ server/test/endpoint/start_all.php
@@ -13,29 +13,52 @@
 	$g_debug = true;
 
 	$_REQUEST = array_merge($_GET, $_POST);
-	$output = array();
-	try {
-		$name = OCP::get_name_from_url($_SERVER['REQUEST_URI']);
-		$url = 'http://' . $_SERVER['HTTP_HOST'] . preg_replace('#(.*' . $name . ')/endpoint.*#', "$1", $_SERVER['REQUEST_URI']);
-		file_get_contents(
-			$url . '/endpoint/start.php?' .
-			'name=' . $name .
-			'&url=' . $url
-		);
-		for ($i = 1; $i < 10; $i++) {
-			$name = 'node' . $i;
-			$node_url = dirname($url) . '/' . $name;
-			file_get_contents(
-				$node_url . '/endpoint/start.php?' .
-				'name=' . $name .
-				'&url=' . $node_url .
-				'&sponsor=' . $url
-			);
+	if (isset($_REQUEST['node_qty'])) {
+		$output = array();
+		try {
+			$name = OCP::get_name_from_url($_SERVER['REQUEST_URI']);
+			$url = 'http://' . $_SERVER['HTTP_HOST'] . preg_replace('#(.*)/' . $name . '/endpoint.*#', "$1", $_SERVER['REQUEST_URI']);
+			for ($i = 0; $i < 10; $i++) {
+				$name = 'node' . $i;
+				$node_url = $url . '/' . $name;
+				if ($i == 0) {
+					$sponsor_url = $node_url;
+					file_get_contents(
+						$node_url . '/endpoint/start.php?' .
+						'name=' . $name .
+						'&url=' . $node_url
+					);
+					continue;
+				}
+
+				file_get_contents(
+					$node_url . '/endpoint/start.php?' .
+					'name=' . $name .
+					'&url=' . $node_url .
+					'&sponsor=' . $sponsor_url
+				);
+			}
+			$output['result'] = 'OK';
+		} catch (Exception $e) {
+			$output['error'] = $e->getMessage();
 		}
-	} catch (Exception $e) {
-		$output['error'] = $e->getMessage();
+		debug_r('OUTPUT', $output);
+		$result = prettyPrint(json_encode($output));
+		echo $result;
+		exit;
 	}
-	debug_r('OUTPUT', $output);
-	$result = prettyPrint(json_encode($output));
-	echo $result;
+	header("Content-Type:text/html; charset=UTF-8;");
 ?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+	</head>
+
+	<body>
+		<form method="GET" action="">
+			Node quantity: <input type="text" name="node_qty" value="10"/><br />
+			<input type="submit" value="Submit" />
+		</form>
+	</body>
+</html>
