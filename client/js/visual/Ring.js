c36b9066e43c99609ad8a6a64e0fdccb000d809a
THOMIAS Yannis <thomias.yannis@gmail.com>
2013-11-25 15:15:07 +0100

    Ring with d3

---
+++ client/js/visual/Ring.js
@@ -0,0 +1,65 @@
+(function(ocp, undefined) {
+	ocp.visual = ocp.visual || {};
+
+	ocp.visual.Ring = function(selector) {
+		this.ring = d3.select(selector);
+		this.width = 450;
+		this.height = 450;
+		this.radius = 200;
+		this.use_title = false;
+		this.minimum = 2;
+
+		this.svg = this.ring.append('svg')
+			.attr("width", this.width)
+			.attr("height", this.height);
+
+		this.svg.append('circle')
+			.attr('cx', this.width / 2)
+			.attr('cy', this.height / 2)
+			.attr('r', this.radius)
+			.attr('class', 'ocp_ring');
+
+
+		this.set = function(data) {
+			var self = this;
+			var dataset = data.map(function(d) {
+				console.log('d=' + d);
+				var value = (parseInt(d.substr(0, 4), 16) / 0xffff) * 2 * Math.PI;
+				console.log('value=' + value);
+				return {
+					value: value,
+					label: d
+				};
+			});
+
+			dataset = dataset.filter(function(d) {
+				return d.value > 0;
+			});
+//			console.log(data);
+//			console.log(dataset);
+
+			var point = this.svg.selectAll('.point').data(dataset);
+			point.enter()
+				.append('g')
+					.attr('transform', this.translate())
+					.classed('point', true);
+
+			point.append('circle')
+					.attr('r', 10)
+					.classed('circle', true);
+
+			point.exit().remove();
+
+		};
+
+		this.translate = function() {
+			var self = this;
+			return function(d) {
+				console.log(d);
+				var x = (self.width / 2) + self.radius * Math.cos(d.value);
+				var y = (self.height / 2) - self.radius * Math.sin(d.value);
+				return 'translate(' + x + ', ' + y + ')';
+			}
+		}
+	};
+})(ocp);
