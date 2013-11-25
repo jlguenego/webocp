f5673f6ec3e92bba560c3442618ff3f0fafc6a26
THOMIAS Yannis <thomias.yannis@gmail.com>
2013-11-25 16:16:00 +0100

    Ring with d3

--- client/js/visual/Ring.js
+++ client/js/visual/Ring.js
@@ -9,6 +9,8 @@
 		this.use_title = false;
 		this.minimum = 2;
 
+		this.ocp_ring = new ocp.dht.Ring();;
+
 		this.svg = this.ring.append('svg')
 			.attr("width", this.width)
 			.attr("height", this.height);
@@ -28,7 +30,7 @@
 				console.log('value=' + value);
 				return {
 					value: value,
-					label: d
+					addr: d
 				};
 			});
 
@@ -42,13 +44,33 @@
 			point.enter()
 				.append('g')
 					.attr('transform', this.translate())
-					.classed('point', true);
+					.attr('data-addr', function(d) { return d.addr; })
+					.classed('ocp_node', true);
 
 			point.append('circle')
 					.attr('r', 10)
 					.classed('circle', true);
 
 			point.exit().remove();
+
+			point.on('mouseover', function(e) {
+				d3.select(this).classed('hover', true);
+			});
+
+			point.on('mouseout', function(e) {
+				d3.select(this).classed('hover', false);
+			});
+
+			point.on('click', function(e) {
+				console.log(d3.select(this));
+				var addr = d3.select(this).attr('data-addr');
+
+				console.log('addr=' + addr);
+				var node = self.ocp_ring.ring[addr];
+				var url = ocp.dht.get_contact_url(node);
+				var win = window.open(url + '/', '_blank');
+ 				win.focus();
+			});
 
 		};
 
